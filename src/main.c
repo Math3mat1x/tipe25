2dbc9e53af597109d693d2df735af9b21e877b8c
RyzenDev <125773543+Ryzen-CS@users.noreply.github.com>
2025-01-16 17:32:46 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -2,6 +2,8 @@
 #include "csvg.h"
 #include <assert.h>
 #include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 #define PI 3.141592
 #define EPSILON 1E-6
@@ -17,10 +19,8 @@ typedef struct fingerprint {
   Ridge** ridges;
 } Fingerprint;
 
-float squared_average_gradient(Image* grad_x, Image* grad_y, int block_size, int x, int y, int* directions) {
-  // TODO : unsatisfactory with blocks_size > 1
-  // somehow applying 2 for instance online take the upper left corner of the image with similar 
-  // level of detail that with blocksize 1
+// Calcule la moyenne carrée des gradients dans un bloc
+float squared_average_gradient(Image* grad_x, Image* grad_y, int block_size, int x, int y, const int* directions) {
   if (!(((x + block_size) < (grad_x->width)) && ((y + block_size) < (grad_y->height)))) {
     return EPSILON;
   }
@@ -28,133 +28,116 @@ float squared_average_gradient(Image* grad_x, Image* grad_y, int block_size, int
   float res = 0;
   for (int i = 0; i < block_size; i++) {
     for (int j = 0; j < block_size; j++) {
-      // Access the red channel for greyscale images
-      float tmp_x = (grad_x->p)[y + j][x + i].r;
-      float tmp_y = (grad_y->p)[y + j][x + i].r;
+      float tmp_x = grad_x->p[y + j][x + i].r;
+      float tmp_y = grad_y->p[y + j][x + i].r;
 
-      if (directions[0] == 1) { 
-        res += tmp_x * tmp_x; // Compute Gxx
-      }
-      if (directions[1] == 1) {
-        res += tmp_x * tmp_y; // Compute Gxy
-      }
-      if (directions[2] == 1) {
-        res += tmp_y * tmp_y; // Compute Gyy
-      }
+      if (directions[0]) res += tmp_x * tmp_x; // Gxx
+      if (directions[1]) res += tmp_x * tmp_y; // Gxy
+      if (directions[2]) res += tmp_y * tmp_y; // Gyy
     }
   }
 
   return res + EPSILON;
 }
 
+// Calcule l'orientation et la cohérence des crêtes/valées
 void ridge_valey_orientation(Image* grad_x, Image* grad_y, int block_size, int x, int y, float* angle, int* coherence) {
-  int* dir1 = malloc(sizeof(int) * 3);
-  int* dir2 = malloc(sizeof(int) * 3);
-  int* dir3 = malloc(sizeof(int) * 3);
-  dir1[0] = 1;
-  dir2[1] = 1;
-  dir3[2] = 1;
+  const int dir1[3] = {1, 0, 0};
+  const int dir2[3] = {0, 1, 0};
+  const int dir3[3] = {0, 0, 1};
 
   float gxx = squared_average_gradient(grad_x, grad_y, block_size, x, y, dir1);
   float gxy = squared_average_gradient(grad_x, grad_y, block_size, x, y, dir2);
   float gyy = squared_average_gradient(grad_x, grad_y, block_size, x, y, dir3);
 
-  *angle = atan((gxx - gyy) / (2 * gxy)) / 2; 
-  printf("%f ", *angle);
-  *coherence = sqrt((gxx - gyy) * (gxx - gyy) + 4 * gxy * gxy) / (gxx + gyy);
-
-  free(dir1);
-  free(dir2);
-  free(dir3);
+  *angle = atan2((gxx - gyy), (2 * gxy)) / 2.0f;
+  *coherence = sqrtf((gxx - gyy) * (gxx - gyy) + 4 * gxy * gxy) / (gxx + gyy);
 }
 
+// Crée une empreinte digitale
 Fingerprint* create_fingerprint(int width, int height) {
-  Fingerprint* res = malloc(sizeof(Fingerprint));
-  res -> width = width;
-  res -> height = height;
-  res -> ridges = malloc(sizeof(Ridge) * height);
+  Fingerprint* fp = malloc(sizeof(Fingerprint));
+  fp->width = width;
+  fp->height = height;
+  fp->ridges = malloc(height * sizeof(Ridge*));
 
   for (int j = 0; j < height; j++) {
-    (res -> ridges)[j] = malloc(sizeof(Ridge) * width);
+    fp->ridges[j] = calloc(width, sizeof(Ridge));
   }
 
-  return res;
+  return fp;
 }
 
+// Libère une empreinte digitale
 void free_fingerprint(Fingerprint* fp) {
-
-  for (int j = 0; j < (fp -> height); j++) {
-    free((fp -> ridges)[j]);
+  for (int j = 0; j < fp->height; j++) {
+    free(fp->ridges[j]);
   }
-  free(fp -> ridges);
+  free(fp->ridges);
   free(fp);
 }
 
+// Normalise la cohérence des crêtes
 void normalize_coherence(Fingerprint* fp) {
-  float max = 0;
-  for (int i = 0; i < (fp -> width); i++) {
-    for (int j = 0; j < (fp -> height); j++) {
-      float tmp = (fp -> ridges)[j][i].coherence;
-      if (tmp > max) {
-        max = tmp;
+  float max = 0.0f;
+
+  // Trouver la cohérence maximale
+  for (int i = 0; i < fp->width; i++) {
+    for (int j = 0; j < fp->height; j++) {
+      if (fp->ridges[j][i].coherence > max) {
+        max = fp->ridges[j][i].coherence;
       }
     }
   }
 
-  for (int i = 0; i < (fp -> width); i++) {
-    for (int j = 0; j < (fp -> height); j++) {
-      float tmp = (fp -> ridges)[j][i].coherence;
-      (fp -> ridges)[j][i].coherence = tmp / max;
+  // Normaliser
+  for (int i = 0; i < fp->width; i++) {
+    for (int j = 0; j < fp->height; j++) {
+      fp->ridges[j][i].coherence /= max;
     }
   }
 }
 
+// Calcule une empreinte digitale à partir d'une image
 Fingerprint* compute_fingerprint(Image* im, int block_size) {
-  // locksize : a divisor of gcd(width, height)
-
-  // compute the gradients using the sobel operator
-  int* sobel_x = malloc(sizeof(int) * 9);
-  int x_values[9] = {1, 0, -1, 2, 0, -2, 1, 0, -1};
-  for (int i = 0; i < 9; i++) (sobel_x[i] = x_values[i]);
-
-  int* sobel_y = malloc(sizeof(int) * 9);
-  int y_values[9] = {1, 2, 1, 0, 0, 0, -1, -2, -1};
-  for (int i = 0; i < 9; i++) (sobel_y[i] = y_values[i]);
+  const int sobel_x[9] = {1, 0, -1, 2, 0, -2, 1, 0, -1};
+  const int sobel_y[9] = {1, 2, 1, 0, 0, 0, -1, -2, -1};
 
   Image* grad_x = ppm_convolution(im, sobel_x, 3);
   Image* grad_y = ppm_convolution(im, sobel_y, 3);
 
-  // initialization of fingerprint
-  int x = im -> width / block_size;
-  int y = im -> height / block_size;
-  Fingerprint* fp = create_fingerprint(x, y);
+  int width = im->width / block_size;
+  int height = im->height / block_size;
+  Fingerprint* fp = create_fingerprint(width, height);
 
-  for (int i = 0; i < x; i++) {
-    for (int j = 0; j < y; j++) {
-      ridge_valey_orientation(grad_x, grad_y, block_size, i, j, &((fp -> ridges)[j][i].angle), &((fp -> ridges)[j][i].coherence));
+  for (int i = 0; i < width; i++) {
+    for (int j = 0; j < height; j++) {
+      ridge_valey_orientation(grad_x, grad_y, block_size, i * block_size, j * block_size, 
+                              &fp->ridges[j][i].angle, &fp->ridges[j][i].coherence);
     }
   }
 
-  free(sobel_x);
-  free(sobel_y);
   ppm_free(grad_x);
   ppm_free(grad_y);
-
   normalize_coherence(fp);
 
   return fp;
 }
 
+// Dessine les crêtes/valées dans un fichier SVG
 void draw_svg(Fingerprint* fp, const char* filename) {
-  int spacing = 20;
-  SVG* svg = svg_init(filename, spacing * (fp -> width), spacing * (fp -> height));
+  const int spacing = 20;
+  SVG* svg = svg_init(filename, spacing * fp->width, spacing * fp->height);
 
-  for (int i = 0; i < (fp -> width); i++) {
-    for (int j = 0; j < (fp -> height); j++) {
-      int color_value = (int)round(fp->ridges[j][i].coherence * 255);
+  for (int i = 0; i < fp->width; i++) {
+    for (int j = 0; j < fp->height; j++) {
+      int color_value = (int)roundf(fp->ridges[j][i].coherence * 255);
       unsigned int color = (color_value << 16) | (color_value << 8) | color_value;
-      float angle = (fp -> ridges)[j][i].angle;
-      svg_line(svg, i * spacing, (j + 1) * spacing, (i + cos(angle)) * spacing, (j + 1 - sin(angle)) * spacing, 10, color);
+      float angle = fp->ridges[j][i].angle;
+
+      svg_line(svg, i * spacing, (j + 1) * spacing, 
+               (i + cosf(angle)) * spacing, (j + 1 - sinf(angle)) * spacing, 
+               10, color);
     }
   }
 
@@ -162,9 +145,22 @@ void draw_svg(Fingerprint* fp, const char* filename) {
 }
 
 int main(int argc, char **argv) {
+  if (argc < 2) {
+    fprintf(stderr, "Usage: %s <input_image>\n", argv[0]);
+    return EXIT_FAILURE;
+  }
+
   Image* im = ppm_open(argv[1]);
-  // TODO : gaussian windowing?
+  if (!im) {
+    fprintf(stderr, "Error: Unable to open image file %s\n", argv[1]);
+    return EXIT_FAILURE;
+  }
+
   Fingerprint* fp = compute_fingerprint(im, 2);
-  draw_svg(fp,"fingerprint.svg");
-  return 0;
+  draw_svg(fp, "fingerprint.svg");
+
+  free_fingerprint(fp);
+  ppm_free(im);
+
+  return EXIT_SUCCESS;
 }
